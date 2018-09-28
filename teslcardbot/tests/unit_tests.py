263e34d8f08021d15d03b77c5bf84ba4a502717f
jrwhitehead <1528969+jrwhitehead@users.noreply.github.com>
2018-09-28 14:07:44 +0100

    Update unit_tests.py

--- teslcardbot/tests/unit_tests.py
+++ teslcardbot/tests/unit_tests.py
@@ -44,8 +44,8 @@ class TestParsingFunctions(unittest.TestCase):
         Card.preload_card_data()
 
         print(str(Card.get_info('tyr')))
-        print(str(Card.get_info('lesser ward')))
-        print(str(Card.get_info('dawnbreaker')))
+        # print(str(Card.get_info('lesser ward')))
+        # print(str(Card.get_info('dawnbreaker')))
 
         self.assertEqual(str(Card.get_info('tyr')), '[📷](http://www.legends-decks.com/img_cards/tyr.png) Tyr '
                                                     '| Creature | 4 - 5/4 | Prophecy, Breakthrough, Guard '
