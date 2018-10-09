2db372fc5a1efff36b4dd6055de65143c7f26637
jrwhitehead <1528969+jrwhitehead@users.noreply.github.com>
2018-10-09 10:46:13 +0100

    Update main.py

--- teslcardbot/main.py
+++ teslcardbot/main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    print('TESLCardBot started! (/r/{})'.format(args.target_sub))
+    print('tesl-bot-9000 started! (/r/{})'.format(args.target_sub))
     bot = TESLCardBot(author='NotGooseFromTopGun', target_sub=args.target_sub)
     bot.start(batch_limit=10, buffer_size=1000)
-    print('TESLCardBot stopped running.')
+    print('tesl-bot-9000 stopped running.')
