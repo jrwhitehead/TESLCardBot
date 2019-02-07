ef6cffaacbfed60ee5e6352a246c1c5ed21a60f8
jrwhitehead <1528969+jrwhitehead@users.noreply.github.com>
2019-02-07 13:24:07 +0000

    Minor update

--- card_handler.py
+++ card_handler.py
@@ -21,20 +21,19 @@ S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-print("card.json_uploader# Uploading cards.json to AWS.")
+print("card_handler# Uploading cards.json to AWS.")
 s3 = boto3.resource('s3')
 s3.meta.client.upload_file('cards.json', S3_BUCKET_NAME, 'cards.json', ExtraArgs={'ACL': "public-read", 'StorageClass': "REDUCED_REDUNDANCY"})
-print('card.json_uploader# All done for now. Going to Sleep.')
 time.sleep(5)
 
 #
 #
 # This section takes the cards.json file as input and outputs a csv file of all card names.
 #
-print('json_csv_converter# Converting files to .csv')
+print('card_handler# Converting files to .csv')
 
 def json2csv(file2convert):
-    print(('json_csv_converter# Reading {}').format(file2convert))
+    print(('card_handler# Reading {}').format(file2convert))
     card_data = open(file2convert,"r")
     cards_parsed = json.load(card_data)
 
@@ -57,12 +56,12 @@ def json2csv(file2convert):
         name = [item.lower() for item in name]
         csvwriter.writerow(name)
         count += 1
-    print(('json_csv_converter# Writing output to {}').format(file2convertcsv))
+    print(('card_handler# Writing output to {}').format(file2convertcsv))
     output_card_data.close()
 
 json2csv('cards.json')
 json2csv('old-cards.json')
-print('json_csv_converter# All done. Going to sleep.')
+print('card_handler# Finished converting and saving files in csv format.')
 time.sleep(2)
 
 # This section find the difference between the csv files..
@@ -70,7 +69,7 @@ time.sleep(2)
 old = set((line.strip() for line in open('old-cards.csv')))
 new = set((line.strip() for line in open('cards.csv')))
 
-print('difference# Checking what\'s different and saving to diff.csv.')
+print('card_handler# Checking what\'s different and saving to diff.csv.')
 with open('diff.csv', 'w') as diff:
     num_of_differences = 0
     for line in new:
@@ -78,8 +77,7 @@ with open('diff.csv', 'w') as diff:
             diff.write('{}\n'.format(line))
             num_of_differences += 1
 
-print(('difference# There were {} new cards to add!').format(num_of_differences))
-print('difference# All done. Going to sleep.')
+print(('card_handler# There were {} new cards in diff.csv!').format(num_of_differences))
 time.sleep(2)
 #
 #
@@ -87,7 +85,6 @@ time.sleep(2)
 #
 # This section takes the diff.csv file of card names as input and downloads the card images from legends-decks.com and uploads the images to AWS.
 #
-print('card_grabber_and_uploader# Reading diff.csv.')
 count = 1
 
 with open ('diff.csv', newline='') as csvfile:
@@ -97,17 +94,19 @@ with open ('diff.csv', newline='') as csvfile:
         split = urllib.parse.urlsplit(url)
         filenamePath = "cards/" + split.path.split("/")[-1]
         filename = split.path.split("/")[-1]
+        print(filename)
         try:
             urllib.request.urlretrieve(url, filename)
         except urllib.error.HTTPError:
             # Do something here to handle the error. For example:
             print("URL", (url, filename), "could not be read.")
             continue
-        print('card_grabber_and_uploader# This is card number:', count)
-        print(('card_grabber_and_uploader# Saving file {} locally').format(filename))
-        print(('card_grabber_and_uploader# Uploading {} to AWS.').format(filename))
+        print('card_handler# This is card number:', count)
+        print(('card_handler# Saving file {} locally').format(filename))
+        print(('card_handler# Uploading {} to AWS.').format(filename))
         s3 = boto3.resource('s3')
         s3.meta.client.upload_file(filename, S3_BUCKET_NAME, filenamePath, ExtraArgs={'ContentType': "image/png", 'ACL': "public-read", 'StorageClass': "REDUCED_REDUNDANCY"})
+        time.sleep(5)
         count += 1
-print('card_grabber_and_uploader# All done. Going to sleep.')
+print('card_handler# Finished.')
 time.sleep(86400)
