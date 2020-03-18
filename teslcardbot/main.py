a2e935304153d8038352331421ca0932f19fd402
jrwhitehead <1528969+jrwhitehead@users.noreply.github.com>
2020-03-18 14:16:52 +0000

    Adding functions

--- teslcardbot/main.py
+++ teslcardbot/main.py
@@ -2,14 +2,17 @@ from bot import TESLCardBot
 import argparse
 from prawcore.exceptions import PrawcoreException
 
-if __name__ == '__main__':
+def log(self, msg):
+    print('tesl-bot-9000 # {}'.format(msg))
+		
+def main():
     parser = argparse.ArgumentParser(description='The Elder Scrolls: Legends bot for Reddit.')
     # No default value to prevent accidental mayhem
     parser.add_argument('-s', '--target_sub', required=True, help='What subreddit will this instance monitor?')
 
     args = parser.parse_args()
 
-    print('tesl-bot-9000 # Started lurking in (/r/{})'.format(args.target_sub))
+    self.log('Started lurking in (/r/{})'.format(args.target_sub))
     bot = TESLCardBot(author='NotGooseFromTopGun', target_sub=args.target_sub)
 	
     try:
@@ -19,4 +22,7 @@ if __name__ == '__main__':
         self.log('Reddit seems to be down! Aborting.')
         self.log(e)
 			
-    print('tesl-bot-9000  # Stopped running.')
+    self.log('Stopped running.')
+	
+if __name__ == '__main__':
+    main()
